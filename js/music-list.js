b91126790ff0908ca38f2817982f2c6b81d1a4d8
mkalavishvili99 <lukamkalavishvili1999@gmail.com>
2024-01-16 13:54:40 +0400

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+let allMusic = [
+  {
+    name: 'Imagine Dragons - Birds',
+    artist: 'Imagine Dragons',
+    img: 'music-1',
+    src: 'music-1',
+  },
+  {
+    name: 'Imagine Dragons - Demons',
+    artist: 'Imagine Dragons',
+    img: 'music-2',
+    src: 'music-2',
+  },
+  {
+    name: 'Imagine Dragons - My Life',
+    artist: 'Imagine Dragons',
+    img: 'music-3',
+    src: 'music-3',
+  },
+  {
+    name: 'My Chemical Romance - Cancer',
+    artist: 'My Chemical Romance',
+    img: 'music-4',
+    src: 'music-4',
+  },
+  {
+    name: 'My Chemical Romance - Helena',
+    artist: 'My Chemical Romance',
+    img: 'music-5',
+    src: 'music-5',
+  },
+  {
+    name: "Queen - Don't Stop Me Now",
+    artist: 'Queen',
+    img: 'music-6',
+    src: 'music-6',
+  },
+  {
+    name: 'XXXTENTACION - What Are You So Afraid Of',
+    artist: 'XXXTENTACION',
+    img: 'music-7',
+    src: 'music-7',
+  },
+  {
+    name: 'Opus - Live is Life',
+    artist: 'Opus',
+    img: 'music-8',
+    src: 'music-8',
+  },
+  {
+    name: 'Andrea Bocelli - Con Te Partirò',
+    artist: 'Andrea Bocelli',
+    img: 'music-9',
+    src: 'music-9',
+  },
+];
